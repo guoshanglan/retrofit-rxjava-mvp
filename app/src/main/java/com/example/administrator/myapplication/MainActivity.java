5377d141bcb7acd8bc3fcbdb7a8286f0478bd669
guoshanglan <gsl86193>
2019-04-22 22:59:51 +0800

    test

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public static void main(String[]args){
+        int []arr={1,2,3,4,5,6,100};
+
+
+
+    }
+
+
+
+
+}
